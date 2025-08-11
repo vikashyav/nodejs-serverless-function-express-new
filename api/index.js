ca41b809cefcdc1bbbf09dada1499975e948e339
vikash1yadav <64723750+vikash1yadav@users.noreply.github.com>
2025-08-11 15:05:01 +0530

    added vercel config 1

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 let items = [];
 
-// Routes ...
+// Routes ...11
 app.get("/", ((req, res) => {
 	const data = "This is base {/index} API. Use the respective routing-API to begin.";
 	return res.status(200).send(data);
